095560a810a088e68fac8accb3b5c01536427914
Santi Arroyo <santi.arroyo551@gmail.com>
2022-09-01 10:46:27 -0400

    commit tree 882b985e30ef163ee39de628d02c647e6bc2e805

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Links from './Links';
 
-function About() {
+function About(prop) {
+  console.log(prop)
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {prop.bio ? <p>{prop.bio}</p> : null}
+      <Links github={prop.links.github} linkedin={prop.links.linkedin}/>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Links(prop) {
+    return (
+      <div>
+        <h3>Links</h3>
+            <a href={prop.github}>{prop.github}</a>
+            <a href={prop.linkedin}>{prop.linkedin}</a>
+        </div>
+    );
+}
+
+
+export default Links;
